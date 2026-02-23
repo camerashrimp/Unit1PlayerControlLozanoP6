f5ab332871008df28c680c2ef59d0692f299033a
camerashrimp <731406@student.dusd.net>
2026-02-23 13:53:36 -0800

    challenge and finish

---
+++ P6PlayerControlLozano/Assets/Course Library/Sky/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontalInput;
+    private float forwardInput;
+
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        //Move the vehicle foward.
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up * turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
